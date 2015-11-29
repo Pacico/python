b9bf4aaa7c417aaabdbc8999536a7f0f1a15b449
Pacico <trin.lai@qq.com>
2015-11-29 16:05:11 +0800

    Create 0000.py

---
+++ Pacico/0000/0000.py
@@ -0,0 +1,27 @@
+# -*- coding:cp936 -*-
+def imgaddingtext(filepath,text,fonttype='arial.ttf',color='black'):
+    from PIL import Image, ImageDraw, ImageFont
+    import os
+    img=Image.open(filepath)
+    x,y=img.size
+    draw=ImageDraw.Draw(img)
+    font=ImageFont.truetype(fonttype,(min(img.size))/5)
+    draw.text((x/2,y/2),text,fill=color,font=font)
+    img.save((os.path.dirname(filepath))+'\\''new.jpg')
+
+#设置变量的默认随机值    
+import random
+listfont=['arial.ttf']
+listcolor=['red','black','brown','white','pink','yellow']
+filepath=raw_input('enter exact path for you file(picture):')
+text=raw_input('enter your text:')
+fonttype=raw_input('enter the font:')
+if fonttype not in listfont:
+    print 'The fonttype you choose does not exit'
+    fonttype=random.choice(listfont)
+color=raw_input('enter the color:')
+if color not in listcolor:
+    print 'The color you choose does not exit'
+    color=random.choice(listcolor)
+imgaddingtext(filepath,text,fonttype,color)
+
